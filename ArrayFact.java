c66a48f37bb756f4dc722fe29cc1a9dd4980ae2a
Jasmine-21 <55360377+Jasmine-21@users.noreply.github.com>
2019-10-14 23:43:21 +0530

    Update ArrayFact.java

--- ArrayFact.java
+++ ArrayFact.java
@@ -1,12 +1,13 @@
+//factorial of number input by user
 import java.util.*;
 public class ArrayFact
 {
 	public static void main(String args[])
 	{
-		Scanner sc=new Scanner(System.in);
-		int res=0;
+		Scanner sc=new Scanner(System.in); //scanner
+		//int res=0;
 		int fact=1;
-		int n=sc.nextInt();
+		int n=sc.nextInt();  //input
 		for(int i=n;i>=1;i--)
 		{
 			fact=fact*i;
